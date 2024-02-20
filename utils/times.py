1935d9cf9b0bf5a73dd2e6023600210f530e799d
chaoyingdeng <chaoyingdeng@gmail18.com>
2024-02-21 02:12:53 +0800

    basic优化

--- utils/times.py
+++ utils/times.py
@@ -5,18 +5,23 @@ from utils.decorator import Decorator
 
 @Decorator.singleton
 class Times:
+    """ 添加单例模式只能避免跨天问题, 无法解决跨月"""
+
+    def __init__(self):
+        self._now = datetime.now()
+
     @property
     def year(self):
-        return datetime.now().strftime(f'%Y')
+        return self._now.strftime(f'%Y')
 
     @property
     def month(self):
-        return datetime.now().strftime(f"%m")
+        return self._now.strftime(f"%m")
 
     @property
     def pre_month(self):
-        return (datetime.now() - relativedelta(month=1)).strftime(f"%m")
+        return (self._now - relativedelta(month=1)).strftime(f"%m")
 
     @property
     def today(self):
-        return datetime.now().strftime(f'%Y-%m-%d')
+        return self._now.strftime(f'%Y-%m-%d')
