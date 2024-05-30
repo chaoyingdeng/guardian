0d3adc5eb871c8ebc145789f3803026c8b837af3
cc <406125295@qq.com>
2024-05-30 18:02:45 +0800

    优化

--- setting.py
+++ setting.py
@@ -5,10 +5,3 @@ password = 'Test123456'
 tenant_id = '20dad5856deb4d33acd962b0a8ed13f5'
 user_id = '8ac274db8d20e2b0018d30141e39039a'
 account_id = '8ac275b97a7bbcfa017a7eceb3d60002'
-
-
-# 邮件配置
-SERVER = 'smtp.qq.com'
-AUTH_CODE = 'rsabmlgwgcoebhfa'
-SENDER = '406125295@qq.com'
-RECEIVERS = '406125295@qq.com, dengchaoying18@gmail.com'
--- utils/emails.py
+++ utils/emails.py
@@ -9,7 +9,7 @@ import sys
 SERVER = 'smtp.qq.com'
 AUTH_CODE = 'rsabmlgwgcoebhfa'
 SENDER = '406125295@qq.com'
-RECEIVERS = '406125295@qq.com, dengchaoying18@gmail.com'
+RECEIVERS = '406125295@qq.com, dengchaoying18@gmail.com, super.chaoying@gmail.com '
 
 
 class Email:
